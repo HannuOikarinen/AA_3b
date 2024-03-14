dc0096781bb2f9ea6f832ae8ac846ca7ac5da7b6
Mikcsu <miika.puominen@gmail.com>
2024-03-14 17:41:59 +0200

    hakee sijainnin ja näyttää sen kartalla

--- app/src/main/java/com/example/aa/MainActivity.kt
+++ app/src/main/java/com/example/aa/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.aa
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,36 +20,190 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aa.ui.theme.AATheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
 
 class MainActivity : ComponentActivity() {
+
+
+    private val permission = arrayOf(
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private var locationRequired: Boolean = false
+
+    override fun onResume() {
+        super.onResume()
+        if (locationRequired) {
+            startLocationUpdates()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        locationCallback?.let {
+            fusedLocationClient?.removeLocationUpdates(it)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        locationCallback?.let {
+            val locationRequest = LocationRequest.Builder(
+                Priority.PRIORITY_HIGH_ACCURACY, 100
+            )
+                .setWaitForAccurateLocation(false)
+                .setMinUpdateIntervalMillis(3000)
+                .setMaxUpdateDelayMillis(100)
+                .build()
+
+            fusedLocationClient?.requestLocationUpdates(
+                locationRequest,
+                it,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        MapsInitializer.initialize(this, MapsInitializer.Renderer.LATEST){
+
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         setContent {
+
+
+            var currentLocation by remember {
+                mutableStateOf(LatLng(0.toDouble(),0.toDouble()))
+            }
+
+            val cameraPosition = rememberCameraPositionState{
+                position = CameraPosition.fromLatLngZoom(
+                    currentLocation, 10f
+                )
+            }
+
+            var cameraPositionState by remember {
+                mutableStateOf(cameraPosition)
+            }
+
+            locationCallback = object: LocationCallback() {
+                override fun onLocationResult(p0: LocationResult) {
+                    super.onLocationResult(p0)
+                    for (location in p0.locations) {
+                        currentLocation = LatLng(location.latitude, location.longitude)
+
+                        cameraPositionState = CameraPositionState(
+                            position = CameraPosition.fromLatLngZoom(
+                                currentLocation, 10f
+                            )
+                        )
+                    }
+                }
+            }
+
             AATheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LocationScreen(this@MainActivity, currentLocation, cameraPositionState)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    private fun LocationScreen(context: Context, currentLocation: LatLng, camerapositioState: CameraPositionState) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AATheme {
-        Greeting("Android")
+        val launchMultiplePermissions = rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) {
+            permissionMaps ->
+            val areGranted = permissionMaps.values.reduce {acc, next -> acc && next}
+            if (areGranted) {
+                locationRequired = true
+                startLocationUpdates()
+                Toast.makeText(context, "Permission Granted!", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(context, "Permission Denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        Box(modifier = Modifier.fillMaxSize()) {
+
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = camerapositioState
+            ) {
+                Marker(
+                    state = MarkerState(
+                        position = currentLocation
+                    ),
+                    title = "You",
+                    snippet = "you're here!!!"
+                )
+            }
+
+
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text = "your location: ${currentLocation.latitude}/${currentLocation.longitude}")
+                Button(onClick = {
+
+                    if (permission.all {
+                        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+                        })
+
+                    {
+                        // get location
+                        startLocationUpdates()
+                    }
+                    else
+                    {
+                        launchMultiplePermissions.launch(permission)
+                    }
+
+                }) {
+                    Text(text = "get your location")
+                }
+            }
+        }
     }
 }
